4c1ec8cdc23eaab10b029067d8374cfeb47c451c
Manuel03-hub <manuel.soddu@iisbuonarrotivolta.it>
2026-05-07 18:00:35 +0000

    bau

--- src/main/java/it/scuola/demopost/DemoApplication.java
+++ src/main/java/it/scuola/demopost/DemoApplication.java
@@ -2,12 +2,18 @@ package it.scuola.demopost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+		Message m = new Message();
+		m.setText("prova da codice");
+		MessageRepository messageRepository = context.getBean(MessageRepository.class);
+		messageRepository.save(m);
 	}
 
 }
